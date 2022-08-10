1c191e536810d52ddf1c4934fa3ac8399c1c8fea
amir-aa <50378368+amir-aa@users.noreply.github.com>
2022-08-10 10:37:34 +0430

    Add files via upload
    Send mail using Zoho SMTP

---
+++ mailByZOHO.py
@@ -0,0 +1,25 @@
+from Crypto.Hash import MD5
+import smtplib,ssl,base64
+from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+SMTPSERVER="smtp.zoho.com"
+SMTPPORT=587
+
+    
+def SendEmail(From:str,To:str,Password:str):
+
+   
+    message=EmailMessage()
+    
+    message.set_content(f"""TEXT""")
+    message["Subject"]="Welcome to site"
+    message['From'] = From
+    message['To'] = To
+    context = ssl.create_default_context()
+    with smtplib.SMTP(SMTPSERVER, SMTPPORT) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(From, Password)
+        server.sendmail(From, To, message.as_string())
+
